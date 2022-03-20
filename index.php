57c6a3c1d49a419ff9d0ecd53cee0ff849dc9bed
Hamza Anwar <88555920+hamzaanwardev@users.noreply.github.com>
2022-03-21 00:16:37 +0500

    updated index.php

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP OOP Inherit</title>
+</head>
+<body>
+    <h1>PHP Inheritance</h1>
+<?php
+class cars {
+    private $price, $color, $model;
+
+    public function __construct($p , $c , $m) {
+        $this->price = $p;
+        $this->color = $c;
+        $this->model = $m;
+
+    }
+    // function __construct() {
+    //     echo "CAR Showroom";
+    // }
+    function details() {
+        echo "this car is of ". $this->price. " Price". "<br>";
+        echo "this car have ". $this->color. " Color". "<br>";
+        echo "The model of this car/truck is ". $this->model. " Modal"."<br>";
+    }
+}
+class trucks extends cars {
+    // function __construct() {
+    //     echo "Truck Showroom";
+    // }
+}
+
+$car = new cars  (500000 , "white" , 2006);
+$truck = new trucks (1100000 , "Red" , 2000);
+$car->details();
+$truck->details();
+?>
+</body>
+</html>
